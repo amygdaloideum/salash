919856c806bc5833d078a7f5e97e160a95d05500
Daniel Bornstrand <daniel.bornstrand@gmail.com>
2016-11-05 20:02:03 +0100

    Fixed typo om mongo url config

--- server/config/config.js
+++ server/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  mongoURL: process.env.MONGO_URL || process.env.MONGO_URL || 'mongodb://localhost:27017/salash',
+  mongoURL: process.env.MONGO_URL || process.env.MONGODB_URI || 'mongodb://localhost:27017/salash',
   port: process.env.PORT || 8000,
   secret: 'hehuhe'
 };
