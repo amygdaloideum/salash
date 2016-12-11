b0bcacfb06e44e525b7d26b6c739c6378ca7f168
Daniel Bornstrand <daniel.bornstrand@gmail.com>
2016-12-11 19:19:39 +0100

    Added production facebook app

--- server/config/auth.js
+++ server/config/auth.js
@@ -6,9 +6,9 @@ import cuid from 'cuid';
 
 export const profiles = {
   'facebookAuth': {
-    'clientID': '1005914192867598', // your App ID
-    'clientSecret': 'a308aec901f2a0317b261e62844a8083', // your App Secret
-    'callbackURL': 'http://localhost:8000/api/auth/facebook/callback'
+    'clientID': process.env.NODE_ENV === 'development' ? '1005914192867598' : '360334164325151', // your App ID
+    'clientSecret': process.env.NODE_ENV === 'development' ? 'a308aec901f2a0317b261e62844a8083' : 'cf933909c844eaadb4ac0e98956644fc', // your App Secret
+    'callbackURL': process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/auth/facebook/callback' : 'http://www.salash.ninja/api/auth/facebook/callback'
   }
 };
 
