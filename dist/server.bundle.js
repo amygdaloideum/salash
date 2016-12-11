9581d0f54551cb242f5e3b937656128b54f64a97
Daniel Bornstrand <daniel.bornstrand@gmail.com>
2016-12-11 21:45:08 +0100

    new production build

--- dist/server.bundle.js
+++ dist/server.bundle.js
@@ -852,7 +852,7 @@
 	function facebookLoginRequest() {
 	  return function (dispatch) {
 	    var settings = "scrollbars=no,toolbar=no,location=no,titlebar=no,directories=no,status=no,menubar=no,width=580,height=400";
-	    var popup = window.open('http://localhost:8000/api/auth/facebook', 'facebook', settings);
+	    var popup = window.open(window.location.origin + '/api/auth/facebook', 'facebook', settings);
 	    return listenForCredentials(popup, dispatch);
 	  };
 	}
@@ -3355,9 +3355,9 @@
 
 	var profiles = exports.profiles = {
 	  'facebookAuth': {
-	    'clientID': '1005914192867598', // your App ID
-	    'clientSecret': 'a308aec901f2a0317b261e62844a8083', // your App Secret
-	    'callbackURL': 'http://localhost:8000/api/auth/facebook/callback'
+	    'clientID': process.env.NODE_ENV === 'development' ? '1005914192867598' : '360334164325151', // your App ID
+	    'clientSecret': process.env.NODE_ENV === 'development' ? 'a308aec901f2a0317b261e62844a8083' : 'cf933909c844eaadb4ac0e98956644fc', // your App Secret
+	    'callbackURL': process.env.NODE_ENV === 'development' ? 'http://localhost:8000/api/auth/facebook/callback' : 'http://www.salash.ninja/api/auth/facebook/callback'
 	  }
 	};
 
